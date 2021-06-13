dc1ef297e58ac7a506ece1a1c5dcc2a39d946660
RoelofNeuman <roelofneuman@gmail.com>
2021-06-13 19:28:12 +0100

    Revert "some sounds"
    This reverts commit feec019ad60d6c0a986a19315862fb94e8d110e3.

--- GMTK 2021/Assets/Scripts/EnemyScript.cs
+++ GMTK 2021/Assets/Scripts/EnemyScript.cs
@@ -9,12 +9,8 @@ public class EnemyScript : MonoBehaviour
     Animator anim;
     public bool moving;
     public bool pushing;
-    public AudioClip Steps;
-    public AudioClip hit;
-    private AudioSource audioSource;
     private void OnEnable()
     {
-        audioSource = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
         anim.SetBool("Moving", moving);
         if (this.transform.position.x < 0)
@@ -74,7 +70,6 @@ public class EnemyScript : MonoBehaviour
             moving = false;
             pushing = true;
             anim.Play("Guard_Push");
-            audioSource.PlayOneShot(hit);
         }
         else if (collision.gameObject.tag == "EnemySpawn")
         {
